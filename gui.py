365f4048511180701294a5780091244833a091b6
rjmtaef <roslanjaver@gmail.com>
2024-09-09 14:43:03 -0300

    Change theme, add clock, handle edit and complete error

--- gui.py
+++ gui.py
@@ -1,6 +1,10 @@
 import functions
 import FreeSimpleGUI as sg
+import time
 
+sg.theme("DarkPurple4")
+
+clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
@@ -10,7 +14,9 @@ edit_button = sg.Button("Edit")
 complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
 
-layout = [[label], [input_box, add_button],
+layout = [[clock],
+          [label],
+          [input_box, add_button],
           [list_box, edit_button, complete_button],
           [exit_button]]
 
@@ -19,9 +25,8 @@ window = sg.Window('My To-Do App',
                    font=('Helvetica', 20))
 
 while True:
-    event, values = window.read()
-    print(event)
-    print(values)
+    event, values = window.read(timeout=10)
+    window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -29,6 +34,7 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
         case "Edit":
+          try:
             todo_to_edit = values['todos'][0]
             new_todo = values['todo']
 
@@ -37,13 +43,18 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+          except IndexError:
+              sg.popup("Please select and item first.", font=('Helvetica', 20))
         case 'Complete':
+          try:
             todo_to_complete = values['todos'][0]
             todos = functions.get_todos()
             todos.remove(todo_to_complete)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
             window['todo'].update(value='')
+          except IndexError:
+              sg.popup("Please select and item first.", font=('Helvetica', 20))
         case 'Exit':
             break
         case 'todos':
